47120dc0968cc5a9ae948191d43ee78153233482
Hugo Lima <limabrot879@gmail.com>
2023-01-24 11:23:41 -0300

    Create ThemeChanged MVVM Message

---
+++ Messages/ThemeChangedMessage.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.Messaging.Messages;
+
+namespace MauiCalculator.Messages;
+
+class ThemeChangedMessage : ValueChangedMessage<string> {
+	public ThemeChangedMessage(string value) : base(value) {}
+}
