f1ef7f8de3680556a3b16d839c1a4af4c2dc7e7f
Hugo Lima <limabrot879@gmail.com>
2023-01-24 11:23:58 -0300

    Create LoadTheme Logic on App class

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,9 +1,43 @@
-﻿namespace MauiCalculator;
+﻿using CommunityToolkit.Mvvm.Messaging;
+using MauiCalculator.Messages;
+
+namespace MauiCalculator;
 
 public partial class App : Application {
+
 	public App() {
 		InitializeComponent();
 
 		MainPage = new AppShell();
+
+		WeakReferenceMessenger.Default.Register<ThemeChangedMessage>(this, (rec, mHnd) => {
+			LoadTheme(mHnd.Value);
+		});
+
+		var theme = Preferences.Get("theme", "System");
+
+		LoadTheme(theme);
+	}
+
+	private void LoadTheme(string theme) {
+		if (!MainThread.IsMainThread) {
+			MainThread.BeginInvokeOnMainThread(() => LoadTheme(theme));
+			return;
+		}
+
+		if (theme == "System") {
+			theme = Current.PlatformAppTheme.ToString();
+		}
+
+		ResourceDictionary dictionary = theme switch {
+			"Light" => new Resources.Styles.Light(),
+			"Dark" => new Resources.Styles.Dark(),
+			_ => null
+		};
+
+		if (dictionary is not null) {
+			Resources.MergedDictionaries.Clear();
+			Resources.MergedDictionaries.Add(dictionary);
+		}
 	}
 }
